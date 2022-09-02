6df563604109a9c4b93e20cf105ca04f5d4fb7f2
Pawan Paudel <pawanpaudel93@gmail.com>
2022-09-02 20:43:04 +0545

    updated according to new version of web-bundlr & web-ardrive

--- web-ardrive.config.js
+++ web-ardrive.config.js
@@ -3,6 +3,7 @@
 const WebArDriveConfig = {
   walletPath: 'wallet.json',
   folderPath: 'dist',
+  appType: 'nuxt',
   parentFolderID: '16659ac0-3739-4bc5-9bcd-433b47aca030',
   destFolderName: 'nft_marketplace',
   production: true,
